c4efe91aae9cbdad584b0610c9893b63fde67c53
Nikesh <nikeshsampathkumar@gmail.com>
2026-06-26 16:45:29 +0530

    nikeshlabday2

---
+++ nikeshlabday2.py
@@ -0,0 +1,114 @@
+#task1
+'''marks=int(input("enter a marks:"))
+if marks>=35:
+    print("pass")
+else:
+    print("fail")'''
+
+#task2
+'''age=int(input("enter age:"))
+if age>=18:
+    print("eligible for voting")
+else:
+    print("not eligible for voting")'''
+
+#task3
+'''bill=float(input("enter a bill:"))
+if bill>=5000:
+    print("bill:",(bill*10/100))
+else:
+    print("final amount:",bill)'''
+
+#task4
+'''user=str(input("enter a user:"))
+password=str(input("enter a password:"))
+if user=="ramkumar" and  password=="12345":
+    print("verify the password")
+else:
+    print("not verify the password")'''
+
+#task5
+'''marks=int(input("enter a marks:"))
+if marks>=90:
+    print("grade A")
+elif marks>=75:
+    print("grade B")
+elif marks>=50:
+    print("grade C")
+else:
+    print("fail")'''
+
+#task6
+'''a=int(input("enter a:"))
+b=int(input("enter b:"))
+cal=input("enter +,-,*,/:")
+if cal=="+":
+    print(a+b)
+elif cal=="-":
+    print(a-b)
+elif cal=="*":
+    print(a+b)
+elif cal=="/":
+    print(a+b)
+else:
+     print("not cal")'''
+
+#task7
+'''units=int(input("enter a units:"))
+if units<=100:
+    print(units*2)
+elif units<=200:
+    print(units*3)
+elif units>=200:
+    print(units*5)
+else:
+    print("not units consum")'''
+
+#task8
+'''age=int(input("enter a age:"))
+if age<=5:
+    print("Ticket free")
+elif age<=12:
+    print("Ticket 50")
+elif age>=12:
+    print("Ticket 100")
+else:
+    print("age:",age)'''
+#task9
+'''salary=int(input("enter a salary:"))
+score=int(input("enter a score:"))
+if salary>=30000 and score>=700:
+    print("the eligible for the lone")
+else:
+    print("not eligible for the lone ")'''
+#task10
+'''percentage=int(input("enter a percentage:"))
+score=int(input("enter a score:"))
+if percentage>=60 and score>=70:
+    print("the eligible for the collage")
+else:
+     print("not eligible for the collage ")'''
+#task11
+'''percentage=int(input("enter a percentage:"))
+aptitude=int(input("enter a aptitude:"))
+if percentage>=65 and aptitude>=75:
+    print("the eligible for the company")
+else:
+     print("not eligible of the company")'''
+#task12
+score=int(input("enter a score:"))
+attendance=int(input("enter a attendance:"))
+if score>=80 and attendance>=75:
+    print("the eligible for the scholarship")
+else:
+     print("not eligible of the scholarship")    
+
+
+
+
+
+        
+     
+    
+
+    
