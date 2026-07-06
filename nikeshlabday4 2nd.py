43f13979bff6b0b272b2e8e9ab876881d2511265
Nikesh <nikeshsampathkumar@gmail.com>
2026-07-06 16:50:13 +0530

    Add files via upload

---
+++ nikeshlabday4 2nd.py
@@ -0,0 +1,160 @@
+#task 1 Break
+'''for i in range(1,10):
+    if i==5:
+        break
+    print(i)'''
+
+#task 2 print number
+'''for i in range (1,100):
+    if i==51:
+        break
+    print(i)'''
+
+#task 3 password
+'''correct_password = "nikesh203"
+while True:
+    password = input("entera password:")
+    if password==correct_password:
+        print("access granted")
+        break
+    else:
+        print("wrong password")'''
+
+#task4
+'''names = ['ram','naresh','nikesh','sakthi']
+search = input("enter a name:")
+for name in names:
+    if name== search:
+        print("name found")
+        break
+else:
+    print("not found")'''
+
+#task5
+'''for i in range(1,11):
+    if i == 5:
+        continue
+    print(i)'''
+
+#task6
+'''for i in range(1,21):
+    if i%3==0:
+        continue
+    print(i)'''
+
+#task7
+'''for i in range(1,51):
+    if i%2==0:
+        continue
+    print(i)'''
+
+#task8
+'''text = input("enter a string:")
+for ch in text:
+    if ch.lower() in "aeiou":
+        continue
+    print(ch,end="")'''
+
+#task9
+'''for i in range(1,11):
+    if i ==5:
+        pass
+    print(i)'''
+
+#task10
+'''def my_function():
+    pass
+print("function executed successfully")'''
+
+#task11
+'''class student:
+    pass
+print("empty class create successfully")'''
+
+#task12
+for i in range(1,7):
+    if i ==3:
+        pass
+    print(i)
+else:
+    print("loop")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
