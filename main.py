48e871b9dc0dbcb30e517499ab98760d55cc0659
Nikesh <nikeshsampathkumar@gmail.com>
2026-07-08 14:20:40 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,55 @@
+#task1
+'''a = "nikesh"
+print(a.upper())
+print(a.lower())
+print(a.title())'''
+from idlelib.outwin import file_line_pats
+
+#task2
+'''email = "nikeshsampathkumar@gmail.com"
+print("starts with a letter:",email[0].isalpha())
+print("ends with .com:",email.endswith(".com"))'''
+
+#task3
+'''letter ="character"
+print(letter.count("a"))
+print(letter.count("e"))'''
+
+#task4
+'''word ="Input"
+print(word.find("p"))
+print(word.find("y"))'''
+
+#task5
+'''password = "nikeshxyx203"
+print(password.isalpha())
+print(password.isdigit())
+print(password.isalnum())'''
+
+#task6
+'''n = "decortion"
+print(n.center(20),"*")
+print(n.ljust(20),"*")
+print(n.rjust(20),"*")'''
+
+#task7
+'''a = "NIKESH IS A GOOD BOY"
+print(a.upper())
+print(a.lower())
+print(a.swapcase())'''
+
+#task 8
+'''a = "I am learnind python"
+print(a.replace("python","java"))'''
+
+#task9
+'''r = ("13")
+print(r.zfill(10))'''
+
+#task10
+ word = "PYTHON"
+ print(word.isupper())
+ print(word.islower())
+ print(word.istitle())
+
+
