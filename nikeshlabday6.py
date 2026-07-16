5aed3918b86d92f67508dd92bc4a0d266b0f86d2
Nikesh <nikeshsampathkumar@gmail.com>
2026-07-16 17:32:09 +0530

    Update nikeshlabday6.py

--- nikeshlabday6.py
+++ nikeshlabday6.py
@@ -4,9 +4,6 @@ num =[1,2,3,4,5,6,7,8,9,10]
 for i in num:
     if i%2==0:
         print(i)'''
-import numbers
-from enum import unique
-from heapq import merge
 
 '''#task2
 a = [10,11,12,13,14,15]
